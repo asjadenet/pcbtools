186a397148b0922cf2c05c75e70ba4902fb21b3d
tiit@infokiir.ee <Tiit leje>
2024-03-18 16:51:21 +0200

    get_prefix_tests added

--- include/pcbtools/get_prefix.h
+++ include/pcbtools/get_prefix.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include <string>
+#include <filesystem>
+
 namespace pcb_tools
 {
   std::string get_prefix();
+  std::string get_prefix(const std::filesystem::path &current_path);
 }
---
+++ test/get_prefix_tests.cpp
@@ -0,0 +1,18 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <pcbtools/get_prefix.h>
+
+TEST_CASE("PCB tests get_prefix empty")
+{
+  REQUIRE(pcb_tools::get_prefix("") == "");
+}
+
+TEST_CASE("PCB tests get_prefix real path")
+{
+  #ifdef WIN32
+  REQUIRE(pcb_tools::get_prefix(R"(C:\Users\tiit\Documents\kicad\amp1)") == "amp1");
+#else
+  REQUIRE(pcb_tools::get_prefix("/mnt/c/Users/tiit/Documents/kicad/amp1") == "amp1");
+#endif
+
+}
