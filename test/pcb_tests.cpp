a14ec8a56ea650d9aa5c369532a52352b423c585
tiit@infokiir.ee <Tiit leje>
2024-03-19 13:23:14 +0200

    new build

--- test/pcb_tests.cpp
+++ test/pcb_tests.cpp
@@ -1,5 +1,4 @@
 #include <catch2/catch_test_macros.hpp>
-
 #include <pcbtools/pcb_library.hpp>
 
 TEST_CASE("PCB tests increments are computed", "[increment]")
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,4 @@
 #include <catch2/catch_test_macros.hpp>
-
 #include <pcbtools/sample_library.hpp>
 
 TEST_CASE("Factorials are computed", "[factorial]")
