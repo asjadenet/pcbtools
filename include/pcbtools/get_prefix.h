114d2056c29c986a476dd6de3405793822c7d19f
tiit@infokiir.ee <Tiit leje>
2024-03-20 15:52:04 +0200

    get_prefix_tests.cpp added

--- include/pcbtools/get_prefix.h
+++ include/pcbtools/get_prefix.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <filesystem>
 #include <string>
-namespace pcb_tools
-{
-  std::string get_prefix();
-}
+
+namespace pcb_tools {
+std::string get_prefix();
+std::string get_prefix(const std::filesystem::path &current_path);
+}// namespace pcb_tools
--- src/pcb_library/get_prefix.cpp
+++ src/pcb_library/get_prefix.cpp
@@ -2,13 +2,17 @@
 #include <filesystem>
 #include <string>
 
-namespace pcb_tools
+namespace pcb_tools {
+std::string get_prefix()
 {
-  std::string get_prefix()
-  {
-    namespace fs = std::filesystem;
-    const fs::path current_path = fs::current_path();
-    std::string prefix = current_path.filename().string();
-    return prefix;
-  }
+  namespace fs = std::filesystem;
+  const fs::path current_path = fs::current_path();
+  return get_prefix(current_path);
 }
+
+std::string get_prefix(const std::filesystem::path &current_path)
+{
+  std::string prefix = current_path.filename().string();
+  return prefix;
+}
+}// namespace pcb_tools
