86585be22e2300601ec0d29f9644a2a3b6dc0b2f
tiit@infokiir.ee <Tiit leje>
2024-03-20 13:59:17 +0200

    lets's try to use newer llvm-17

--- test/constexpr_tests.cpp
+++ test/constexpr_tests.cpp
@@ -4,6 +4,7 @@
 TEST_CASE("Factorials are computed with constexpr", "[factorial]")
 {
   STATIC_REQUIRE(factorial_constexpr(0) == 1);
+  //recursion can cause segmentation fault sometimes
   //STATIC_REQUIRE(factorial_constexpr(1) == 1);
   //STATIC_REQUIRE(factorial_constexpr(2) == 2);
   //STATIC_REQUIRE(factorial_constexpr(3) == 6);
