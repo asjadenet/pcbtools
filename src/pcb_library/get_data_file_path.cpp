1008ba0cca027f73caa878e6adbf25cb3906804f
tiit@infokiir.ee <Tiit leje>
2024-03-25 22:21:39 +0200

    let's try to avoid direct calling free

--- src/pcb_library/get_data_file_path.cpp
+++ src/pcb_library/get_data_file_path.cpp
@@ -6,27 +6,38 @@
 
 
 namespace pcb_tools {
+
+#if defined(_MSC_VER)
+// Custom deleter that uses free. This encapsulates the call to free in a RAII manner.
+auto freeDeleter = [](char* ptr) { free(ptr); };
+
+// Wrap the C string in a unique_ptr with the custom deleter.
+using cstring_uptr = std::unique_ptr<char, decltype(freeDeleter)>;
+#endif
+
 std::string getEnvVar(const char *name)
 {
+  std::string result;
 #if defined(_MSC_VER)
-  char *value = nullptr;
+  char *rawValue = nullptr;
   size_t size = 0;
-  if (_dupenv_s(&value, &size, name) == 0 && value != nullptr) {
-    std::string result(value);
-    free(value);// Still need to free memory allocated by _dupenv_s
-    return result;
+  // _dupenv_s requires a call to free, so we ensure it's managed by a smart pointer.
+  if (_dupenv_s(&rawValue, &size, name) != 0 || rawValue == nullptr) {
+    throw std::runtime_error("Failed to get environment variable.");
   }
+  cstring_uptr value(rawValue, freeDeleter);
+  result = value.get();
 #else
   const char *value = std::getenv(name);// NOLINT(concurrency-mt-unsafe)
-  if (value != nullptr) { return { value }; }
+  if (value != nullptr) {   result =  value; }
 #endif
-  throw std::runtime_error("Environment variable not set or error retrieving it.");
+  return result;
 }
 
 std::filesystem::path get_data_file_path(const std::filesystem::path &file_name)
 {
   const auto basePath = getEnvVar("PCB_TOOLS_DATA");
-  if (basePath.empty()) { throw std::runtime_error("Environment variable 'MY_TEXT_FILES_PATH' not set."); }
+  if (basePath.empty()) { throw std::runtime_error("Environment variable 'PCB_TOOLS_DATA' not set."); }
 
   return std::filesystem::path(basePath) / file_name;
 }
